0c42115f24afd50c0a875c5460151a1e30c99946
stossthegreat <daazawaaza@proton.me>
2025-09-20 03:26:30 +0100

    Remove accidentally created backend files from frontend repo

--- src/modules/alarms/alarms.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AlarmsController } from "./alarms.controller";
-import { AlarmsService } from "./alarms.service";
-
-@Module({
-  controllers: [AlarmsController],
-  providers: [AlarmsService],
-})
-export class AlarmsModule {}
--- src/modules/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthGuard } from './auth.guard';
-
-@Module({
-  providers: [AuthGuard],
-  exports: [AuthGuard],
-})
-export class AuthModule {} 
